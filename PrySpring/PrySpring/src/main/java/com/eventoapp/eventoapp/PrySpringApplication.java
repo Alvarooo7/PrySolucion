12503c8354961063c5203422072ad96f84170ef7
Alvaro <alvarofranco2020@gmail.com>
2019-09-02 14:07:35 -0500

    Version1

---
+++ PrySpring/PrySpring/src/main/java/com/eventoapp/eventoapp/PrySpringApplication.java
@@ -0,0 +1,13 @@
+package com.eventoapp.eventoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrySpringApplication.class, args);
+	}
+
+}
